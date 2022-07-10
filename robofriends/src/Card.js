a5d552581e6739a90b2a9842c81635a394ffec2f
ralexrivero <ralexrivero@gmail.com>
2022-07-09 23:40:58 -0300

    add card css

--- robofriends/src/Card.js
+++ robofriends/src/Card.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './card.css'
 
 const Card = ({name, email, id}) => {
     return (
-        <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
+        <div className="robo-card tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
             <img src={`https://robohash.org/${id}?size=200x200`} alt="Robot profile"/>
             <div>
                 <h2 className="">{name}</h2>
