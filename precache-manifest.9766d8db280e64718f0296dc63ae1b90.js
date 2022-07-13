19dbce5cb5917164ccc0db941ca258e49cbf5b07
ralexrivero <ralexrivero@gmail.com>
2022-07-13 06:01:04 +0000

    Updates

---
+++ precache-manifest.9766d8db280e64718f0296dc63ae1b90.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "00eab4ddd26d052b38b5",
+    "url": "/robofriends/static/css/main.fd75a6b2.chunk.css"
+  },
+  {
+    "revision": "00eab4ddd26d052b38b5",
+    "url": "/robofriends/static/js/main.00eab4dd.chunk.js"
+  },
+  {
+    "revision": "8f2f4211b0891106f089",
+    "url": "/robofriends/static/js/1.8f2f4211.chunk.js"
+  },
+  {
+    "revision": "65f68f2707c4f2020761",
+    "url": "/robofriends/static/css/2.ef0478d4.chunk.css"
+  },
+  {
+    "revision": "65f68f2707c4f2020761",
+    "url": "/robofriends/static/js/2.65f68f27.chunk.js"
+  },
+  {
+    "revision": "9450bc94f1ebca8b96a9",
+    "url": "/robofriends/static/js/runtime~main.9450bc94.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "155f8d174f02fe9a83ef1dd6322f465c",
+    "url": "/robofriends/index.html"
+  }
+];
